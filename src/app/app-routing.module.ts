bf3995e425efd34a29ee57ae8f59cadc3baff741
Sebastian Amin Fariña <amiin74@gmail.com>
2022-05-13 19:13:55 -0300

    Initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HeroComponent } from './components/hero/hero.component';
+import { AboutComponent } from './components/about/about.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { WorksComponent } from './components/works/works.component';
+import { ContactComponent } from './components/contact/contact.component';
+
+const appRoutes:Routes = [
+  {path: '', component: HeroComponent},
+  {path: 'hero', component: HeroComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'experiencie', component: ExperienceComponent},
+  {path: 'works', component: WorksComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: '**', component: HeroComponent}
+]
+
+@NgModule({
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
